2e8a90deecfefa28819a9148e93a2dc192efbe05
heejung <gimm9567@gmail.com>
2025-02-10 18:15:05 +0900

    feat: add login

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import AllPosts from "../pages/posts/all";
 import PostsByInstitution from "../pages/posts/institution";
 import PostsByDate from "../pages/posts/date";
+import LoginForm from "../pages/users/login";
 
 const RoutesConfig = () => {
   return (
@@ -9,6 +10,7 @@ const RoutesConfig = () => {
       <Route path="/posts" element={<AllPosts />} />
       <Route path="/posts/:institution" element={<PostsByInstitution />} />
       <Route path="/posts/:year/:month" element={<PostsByDate />} />
+      <Route path="/users/login" element={<LoginForm />} />
     </Routes>
   );
 };
