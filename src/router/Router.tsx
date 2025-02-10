050e338b602088347053825cd82b09f3105b9538
heejung <gimm9567@gmail.com>
2025-02-10 15:11:24 +0900

    feat: modify router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import AllPosts from "../pages/posts/all";
 import PostsByInstitution from "../pages/posts/institution";
 import PostsByDate from "../pages/posts/date";
+import SignupForm from "../pages/users/register";
 
 const RoutesConfig = () => {
   return (
@@ -9,6 +10,7 @@ const RoutesConfig = () => {
       <Route path="/posts" element={<AllPosts />} />
       <Route path="/posts/:institution" element={<PostsByInstitution />} />
       <Route path="/posts/:year/:month" element={<PostsByDate />} />
+      <Route path="/users/register" element={<SignupForm />} />
     </Routes>
   );
 };
