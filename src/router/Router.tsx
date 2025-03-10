f14fefe13aa8054995106a0750cd98f1ad206ee6
k-jayeoneee <137jayeon0322@naver.com>
2025-03-10 16:47:50 +0900

    feat: add mypage/manager api

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -8,7 +8,6 @@ import EditPage from "../pages/volunteer/edit";
 import PostPage from "../pages/volunteer/post";
 import LoginForm from "../pages/users/login";
 import CrowdFunding from "../pages/crowdfunding/all";
-import OrganPage from "../pages/mypage/manager/organ";
 import BenePage from "../pages/mypage/manager/bene";
 import FundingPage from "../pages/mypage/manager/funding";
 import PostRequestPage from "../pages/mypage/manager/post-request";
@@ -18,6 +17,7 @@ import SponserAnnouncePage from "../pages/mypage/sponser/announce";
 import PersonalEditPage from "../pages/mypage/sponser/personal-edit";
 import VolunteerPage from "../pages/volunteer/main";
 import ScrapPage from "../pages/mypage/sponser/scrap";
+import SponPage from "../pages/mypage/manager/spon";
 
 const RoutesConfig = () => {
   return (
@@ -34,8 +34,8 @@ const RoutesConfig = () => {
       <Route path="/users/login" element={<LoginForm />} />
       <Route path="/crowdfunding/all" element={<CrowdFunding />} />
       <Route path="/volunteer" element={<VolunteerPage />} />
-      <Route path="/manager/organ" element={<OrganPage />} />
       <Route path="/manager/bene" element={<BenePage />} />
+      <Route path="/manager/spon" element={<SponPage />} />
       <Route path="/manager/funding" element={<FundingPage />} />
       <Route path="/manager/post-request" element={<PostRequestPage />} />
       <Route path="/sponser/funding" element={<SponserFundingPage />} />
