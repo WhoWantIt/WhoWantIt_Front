9ea5ec80a30ec7fa726218df0bdf67463d60c8eb
heejung <gimm9567@gmail.com>
2025-03-15 10:38:59 +0900

    feat: add dafalut mypage

--- src/pages/mypage/sponser/mypage.tsx
+++ src/pages/mypage/sponser/mypage.tsx
@@ -4,9 +4,36 @@ import styled from "styled-components";
 import Navigation from "../../../components/Navigation";
 import api from "../../../utils/api";
 
+const fieldLabels: Record<string, string> = {
+  name: "이름",
+  role: "직함",
+  nickname: "닉네임",
+  email: "이메일",
+  phoneNumber: "전화번호",
+  address: "주소",
+};
+
 const PersonalPage = () => {
   const [userInfo, setUserInfo] = useState(null);
   const navigate = useNavigate();
+  const [documents] = useState<string[]>([
+    "스크랩",
+    "참여한 펀딩",
+    "참여한 봉사",
+    "마이페이지",
+  ]);
+  const [activeDoc, setActiveDoc] = useState<string | null>(null);
+
+  const handleNavigation = (doc: string) => {
+    setActiveDoc(doc);
+    const routes: { [key: string]: string } = {
+      스크랩: "/sponser/scrap/funding",
+      "참여한 펀딩": "/sponser/funding",
+      "참여한 봉사": "/sponser/volunteer",
+      마이페이지: "/sponser/mypage",
+    };
+    navigate(routes[doc]);
+  };
 
   useEffect(() => {
     const fetchUserInfo = async () => {
@@ -31,10 +58,15 @@ const PersonalPage = () => {
         <Sidebar>
           <ButtonWrapper>마이페이지</ButtonWrapper>
           <DocumentList>
-            <DocumentItem>스크랩</DocumentItem>
-            <DocumentItem>참여한 펀딩</DocumentItem>
-            <DocumentItem>참여한 봉사</DocumentItem>
-            <DocumentItem>개인정보</DocumentItem>
+            {documents.map((doc, index) => (
+              <DocumentItem
+                key={index}
+                active={activeDoc === doc}
+                onClick={() => handleNavigation(doc)}
+              >
+                {doc}
+              </DocumentItem>
+            ))}
           </DocumentList>
         </Sidebar>
         <MainContent>
@@ -47,7 +79,7 @@ const PersonalPage = () => {
                 key !== "password" &&
                 key !== "image" && (
                   <ProfileItem key={key}>
-                    <Subtitle>{key}</Subtitle>
+                    <Subtitle>{fieldLabels[key] || key}</Subtitle>
                     <ProfileValue>{value}</ProfileValue>
                   </ProfileItem>
                 ),
@@ -79,38 +111,52 @@ const Sidebar = styled.div`
   align-items: center;
   height: 100vh;
 `;
+
 const ButtonWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   gap: 10px;
-  margin-top: 30px;
+  margin-top: 50px;
+  font-size: 25px;
+  font-weight: bold;
 `;
+
 const DocumentList = styled.ul`
   list-style: none;
-  border-bottom: 2px solid #ffffff;
+  border-bottom: 1px solid #ffffff;
   padding: 0;
-  margin-top: 100px;
+  margin-top: 50px;
   width: 250px;
 `;
-const DocumentItem = styled.li`
-  background-color: #3e5879;
+interface DocumentItemProps {
+  active: boolean;
+}
+
+const DocumentItem = styled.li.withConfig({
+  shouldForwardProp: (prop) => prop !== "active",
+})<DocumentItemProps>`
+  background-color: ${({ active }) => (active ? "#adacc2" : "#3e5879")};
   cursor: pointer;
   &:hover {
     background-color: #adacc2;
   }
-  border-top: 2px solid white;
+  border-top: 1px solid #ffffff;
   height: 55px;
   display: flex;
   align-items: center;
   justify-content: center;
+  color: white;
+  font-size: 18px;
 `;
+
 const MainContent = styled.div`
   flex: 1;
   display: flex;
   justify-content: center;
 `;
+
 const ProfileWrapper = styled.div`
   width: 50%;
   text-align: center;
@@ -118,6 +164,7 @@ const ProfileWrapper = styled.div`
   flex-direction: column;
   align-items: center;
 `;
+
 const Title = styled.h2`
   font-size: 24px;
   font-weight: bold;
@@ -125,6 +172,7 @@ const Title = styled.h2`
   margin-bottom: 30px;
   color: #3e5879;
 `;
+
 const ProfileItem = styled.div`
   width: 80%;
   display: flex;
@@ -132,14 +180,17 @@ const ProfileItem = styled.div`
   margin-bottom: 10px;
   font-size: 16px;
 `;
+
 const Subtitle = styled.div`
   color: #3e5879;
   font-weight: bold;
   margin-bottom: 5px;
 `;
+
 const ProfileValue = styled.div`
   color: #555;
 `;
+
 const Button = styled.button`
   width: 100px;
   height: 40px;
@@ -151,6 +202,7 @@ const Button = styled.button`
   cursor: pointer;
   margin-top: 30px;
 `;
+
 const CardImage = styled.img`
   width: 150px;
   height: 150px;
